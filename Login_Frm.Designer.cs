738dce954fca931c30055f0693bdbc157f4584bf
jh ma <weixiao98@hotmail.com>
2020-12-15 14:07:53 +0800

    添加项目文件。

---
+++ Login_Frm.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace MyQQ
+{
+    partial class Login_Frm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.closeBtn = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.closePBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.closePBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(97, 172);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(238, 22);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Location = new System.Drawing.Point(97, 211);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.PasswordChar = '*';
+            this.textBox1.Size = new System.Drawing.Size(238, 16);
+            this.textBox1.TabIndex = 1;
+            // 
+            // closeBtn
+            // 
+            this.closeBtn.BackColor = System.Drawing.Color.Transparent;
+            this.closeBtn.BackgroundImage = global::MyQQ.Properties.Resources.close1;
+            this.closeBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.closeBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.closeBtn.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.closeBtn.Location = new System.Drawing.Point(336, 51);
+            this.closeBtn.Margin = new System.Windows.Forms.Padding(0);
+            this.closeBtn.Name = "closeBtn";
+            this.closeBtn.Size = new System.Drawing.Size(20, 20);
+            this.closeBtn.TabIndex = 2;
+            this.closeBtn.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.BackgroundImage = global::MyQQ.Properties.Resources.min1;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.Transparent;
+            this.button1.Location = new System.Drawing.Point(374, 6);
+            this.button1.Margin = new System.Windows.Forms.Padding(0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(20, 20);
+            this.button1.TabIndex = 3;
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // closePBox
+            // 
+            this.closePBox.BackColor = System.Drawing.Color.Transparent;
+            this.closePBox.Image = global::MyQQ.Properties.Resources.close1;
+            this.closePBox.Location = new System.Drawing.Point(401, 4);
+            this.closePBox.Name = "closePBox";
+            this.closePBox.Size = new System.Drawing.Size(24, 24);
+            this.closePBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.closePBox.TabIndex = 4;
+            this.closePBox.TabStop = false;
+            this.closePBox.MouseClick += new System.Windows.Forms.MouseEventHandler(this.closePBox_MouseClick);
+            this.closePBox.MouseLeave += new System.EventHandler(this.closePBox_MouseLeave);
+            this.closePBox.MouseHover += new System.EventHandler(this.closePBox_MouseHover);
+            // 
+            // Login_Frm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Highlight;
+            this.BackgroundImage = global::MyQQ.Properties.Resources.login_1;
+            this.ClientSize = new System.Drawing.Size(427, 327);
+            this.Controls.Add(this.closePBox);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.closeBtn);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Login_Frm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.TransparencyKey = System.Drawing.Color.Transparent;
+            ((System.ComponentModel.ISupportInitialize)(this.closePBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button closeBtn;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox closePBox;
+    }
+}
+
---
+++ Login_Frm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyQQ
+{
+    public partial class Login_Frm : Form
+    {
+        public Login_Frm()
+        {
+            InitializeComponent();
+        }
+
+        private void closePBox_MouseClick(object sender, MouseEventArgs e)
+        {
+            Close();
+        }
+
+        private void closePBox_MouseHover(object sender, EventArgs e)
+        {
+            closePBox.BackColor = Color.FromArgb(150, 50, 50);
+        }
+
+        private void closePBox_MouseLeave(object sender, EventArgs e)
+        {
+            closePBox.BackColor = Color.Transparent;
+        }
+    }
+}
